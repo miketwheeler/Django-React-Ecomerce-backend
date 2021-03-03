3b2d932db5afb900568ac5732d183039613915b4
miketwheeler <mikeytwheeler@gmail.com>
2021-03-03 12:58:50 -0800

    altered cors setting altered products descr

--- backend/settings.py
+++ backend/settings.py
@@ -39,11 +39,14 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
 	'rest_framework',
+	'corsheaders',
 	
 	'base.apps.BaseConfig',
 ]
 
 MIDDLEWARE = [
+	'corsheaders.middleware.CorsMiddleware',
+
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -122,3 +125,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# whitelisted adresses - needs to be set
+# CORS_ALLOWED_ORIGINS = [
+# 	"http://localhost:3000/",
+# 	"http://localhost:8000/",
+# ]
+
+# allows all, open whitelist - kinda risky <<<<< TEMPORARY/TESTING
+CORS_ALLOW_ALL_ORIGINS = True
