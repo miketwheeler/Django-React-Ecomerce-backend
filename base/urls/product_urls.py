8aff9e6b1d673962edb286c9b04be1d73aa9c4ca
miketwheeler <mikeytwheeler@gmail.com>
2021-04-03 13:27:02 -0700

    routes, views, and model for create & update prod

--- base/urls/product_urls.py
+++ base/urls/product_urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 from base.views import product_views as views
 
+# url patterns for products 
 urlpatterns = [
+	# paths for shop browsing
 	path('', views.getProducts, name="products"),
-	path('<str:pk>/', views.getProduct, name="product"),
 
+	# CRUD paths for products
+	path('create/', views.createProduct, name="product-create"),
+	path('<str:pk>/', views.getProduct, name="product"),
+	path('update/<str:pk>/', views.updateProduct, name="product-update"),
 	path('delete/<str:pk>/', views.deleteProduct, name="product-delete"),
 ]
